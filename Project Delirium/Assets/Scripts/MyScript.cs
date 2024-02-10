c561929e471cf447a87b93754ca679d97accb61f
Dani <varga.daniel.daniel@gmail.com>
2024-02-10 01:40:23 +0100

    Added Unity project

---
+++ Project Delirium/Assets/Scripts/MyScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Delirium.MyFirstNamespace
+{
+    public class MyScript : ScriptableObject
+    {
+        public bool isProvince = false;
+    }
+}
